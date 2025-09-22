6b69e70d3e83fdebd693e78f04d6c7077142cc45
TheMythologist <leekaixuan2001@gmail.com>
2025-09-22 09:08:49 +0800

    Update Forge maker for MacOS

--- forge.config.ts
+++ forge.config.ts
@@ -1,8 +1,8 @@
 import type { ForgeConfig } from '@electron-forge/shared-types';
 import { MakerSquirrel } from '@electron-forge/maker-squirrel';
-import { MakerZIP } from '@electron-forge/maker-zip';
 import { MakerDeb } from '@electron-forge/maker-deb';
 import { MakerRpm } from '@electron-forge/maker-rpm';
+import { MakerPKG } from '@electron-forge/maker-pkg';
 import { AutoUnpackNativesPlugin } from '@electron-forge/plugin-auto-unpack-natives';
 import { WebpackPlugin } from '@electron-forge/plugin-webpack';
 import { FusesPlugin } from '@electron-forge/plugin-fuses';
@@ -17,7 +17,7 @@ const config: ForgeConfig = {
     extraResource: ['service-account.json'],
   },
   rebuildConfig: {},
-  makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
+  makers: [new MakerSquirrel(), new MakerPKG(), new MakerRpm(), new MakerDeb()],
   plugins: [
     new AutoUnpackNativesPlugin({}),
     new WebpackPlugin({
